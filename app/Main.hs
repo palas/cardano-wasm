50c266e8e0971b7112ae3f223408f1ff0aa33d8f
Pablo Lamela <pablo.lamela@iohk.io>
2025-05-12 19:33:06 +0200

    Apply @amesgen suggested fix and uncomment example

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
---import Data.Function ((&))
+import Data.Function ((&))
 import qualified Cardano.Api as Api
---import qualified Cardano.Api.Ledger as Ledger
---import qualified Cardano.Api.Experimental as Exp
+import qualified Cardano.Api.Ledger as Ledger
+import qualified Cardano.Api.Experimental as Exp
 import qualified Cardano.Api.Internal.Script as Script
 
 main :: IO ()
@@ -24,22 +24,15 @@ main = do
                 Api.TxOutDatumNone
                 Script.ReferenceScriptNone
   let txFee = Api.TxFeeExplicit sbe 2_000_000
-  print txFee
--- The following code doesn't link:
---
--- wasm-ld: error: ~/.ghc-wasm/.cabal/store/ghc-9.10.1.20250327-inplace/crdn-crypt-prs-2.2.1.0-b6ea296a/lib/libHScrdn-crypt-prs-2.2.1.0-b6ea296a.a(Praos.o): undefined symbol: crypto_vrf_ietfdraft03_seedbytes
 
---
---
---  let txBodyContent = Api.defaultTxBodyContent sbe
---                    & Api.setTxIns [txIn]
---                    & Api.setTxOuts [txOut]
---                    & Api.setTxFee txFee
---print txBodyContent
--- let (Right txBody) = Api.createTransactionBody sbe txBodyContent
--- print txBody
--- let (Right signingKey) = Api.deserialiseFromBech32 (Api.AsSigningKey Api.AsPaymentKey) "addr_sk1648253w4tf6fv5fk28dc7crsjsaw7d9ymhztd4favg3cwkhz7x8sl5u3ms"
--- let witness = Api.WitnessPaymentKey signingKey
--- let oldApiSignedTx :: Api.Tx Api.ConwayEra = Api.signShelleyTransaction sbe txBody [witness]
--- print oldApiSignedTx
+
+  let txBodyContent = Api.defaultTxBodyContent sbe
+                    & Api.setTxIns [txIn]
+                    & Api.setTxOuts [txOut]
+                    & Api.setTxFee txFee
+  let (Right txBody) = Api.createTransactionBody sbe txBodyContent
+  let (Right signingKey) = Api.deserialiseFromBech32 (Api.AsSigningKey Api.AsPaymentKey) "addr_sk1648253w4tf6fv5fk28dc7crsjsaw7d9ymhztd4favg3cwkhz7x8sl5u3ms"
+  let witness = Api.WitnessPaymentKey signingKey
+  let oldApiSignedTx :: Api.Tx Api.ConwayEra = Api.signShelleyTransaction sbe txBody [witness]
+  print oldApiSignedTx
 
