35bff09e01fd7b31afa3d54196051072412656bb
Pablo Lamela <pablo.lamela@iohk.io>
2025-05-21 16:02:58 +0200

    Get strings passed to JS

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,15 @@
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-module Main where
+module Main (add42, sayNum, main) where
 
-import Data.Function ((&))
-import qualified Cardano.Api as Api
-import qualified Cardano.Api.Ledger as Ledger
-import qualified Cardano.Api.Experimental as Exp
-import qualified Cardano.Api.Internal.Script as Script
+import GHC.Wasm.Prim
+
+foreign export javascript "add42"
+    add42 :: Int -> IO Int
+add42 x = return (x + 42)
+
+foreign export javascript "sayNum"
+    sayNum :: Int -> IO JSString
+sayNum x = return (toJSString ("The number is: " ++ show x ++ "!"))
 
 main :: IO ()
-main = do
-  let sbe :: Api.ShelleyBasedEra Api.ConwayEra = Api.shelleyBasedEra
-  let (Right srcTxId) = Api.deserialiseFromRawBytesHex Api.AsTxId "be6efd42a3d7b9a00d09d77a5d41e55ceaf0bd093a8aa8a893ce70d9caafd978"
-  let srcTxIx = Api.TxIx 0
-  let txIn = ( Api.TxIn srcTxId srcTxIx
-             , Api.BuildTxWith (Api.KeyWitness Api.KeyWitnessForSpending)
-             )
-  let (Just destAddress) = Api.deserialiseAddress (Api.AsAddressInEra $ Api.AsConwayEra) "addr_test1vzpfxhjyjdlgk5c0xt8xw26avqxs52rtf69993j4tajehpcue4v2v"
-  let txOut = Api.TxOut
-                destAddress
-                (Api.lovelaceToTxOutValue sbe 10_000_000)
-                Api.TxOutDatumNone
-                Script.ReferenceScriptNone
-  let txFee = Api.TxFeeExplicit sbe 2_000_000
-
-
-  let txBodyContent = Api.defaultTxBodyContent sbe
-                    & Api.setTxIns [txIn]
-                    & Api.setTxOuts [txOut]
-                    & Api.setTxFee txFee
-  let (Right txBody) = Api.createTransactionBody sbe txBodyContent
-  let (Right signingKey) = Api.deserialiseFromBech32 (Api.AsSigningKey Api.AsPaymentKey) "addr_sk1648253w4tf6fv5fk28dc7crsjsaw7d9ymhztd4favg3cwkhz7x8sl5u3ms"
-  let witness = Api.WitnessPaymentKey signingKey
-  let oldApiSignedTx :: Api.Tx Api.ConwayEra = Api.signShelleyTransaction sbe txBody [witness]
-  print oldApiSignedTx
+main = pure ()
 
